22ec061db754b71bd7c813aec47811ea8d30b1e4
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-01 13:38:08 +0530

    Create admin.py

---
+++ plugins/admin.py
@@ -0,0 +1,22 @@
+
+# (c) @AbirHasan2005
+
+import traceback
+import os
+
+from pyrogram import Client as Tellybots
+from pyrogram import filters
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from config import Config
+else:
+    from config import Config
+from database.access import tellybots
+
+@Tellybots.on_message(filters.private & filters.command('total'))
+async def sts(c, m):
+    if m.from_user.id != Config.OWNER_ID:
+        return 
+    total_users = await tellybots.total_users_count()
+    await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
+
