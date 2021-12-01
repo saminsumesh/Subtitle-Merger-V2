56d35a30a273ffb8d60a7125250134903b6c3a65
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-01 09:01:41 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,64 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+class Translation(object):
+
+    START_TEXT = """
+<b>Hey</b><b> {} </b>
+
+<b>I am Telegram Most Powerful Url Uploader Bot</b>
+
+<b>I can Upload Any Link in File or Video</b>
+
+<b>Use Help Command to Know How to Use me</b>
+
+<b>Made With 💕 By</b><b> @Tellybots_4u</b>
+"""
+    HELP_TEXT = """
+<b>Link to Media or File</b>
+➠ <b>Send a link for upload to telegram file or media.</b>
+
+<b>Set Thumbnail</b>
+➠ <b>Send a photo to make it as permanent thumbnail.</b>
+
+<b>Deleting Thumbnail</b>
+➠ <b>Send /delthumb to delete thumbnail.</b>
+
+<b>Show Thumbnail</b>
+➠ <b>Send /showthumb to view custom thumbnail.</b>
+
+<b>Made With 💕 By</b><b> @Tellybots_4u</b>
+"""
+    ABOUT_TEXT = """
+ **🤖 <b>Bot :** URL Uploader</b>\n
+ **👲 <b>Developer :** [Tellybots_4u](https://telegram.me/tellybots_4u)</b>\n
+ **👥 <b>Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)</b>\n
+ **❄️ <b>Credits :** Everyone in this journey</b>\n
+ **🍴 <b>Source :** [Click here](https://t.me/tellybots_digital)</b>\n
+ **📝 <b>Language :** [Python3](https://python.org)</b>\n
+ **📚 <b>Library :** [Pyrogram v1.2.0](https://pyrogram.org)</b>\n
+ **🌟 <b>Server :** [Heroku](https://heroku.com)</b>\n
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
+        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
+        ],[
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
 
