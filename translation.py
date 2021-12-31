7706e8b4884a0983c5b8a089f6d06041198d1c15
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-31 10:15:36 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,12 @@
+
+class Translation:
+
+    DOWNLOAD_SUCCESS = """File downloaded successfully!
+
+Time taken : {} seconds."""
+    FILE_SIZE_ERROR = "ERROR ❌❌❌ : Sorry I Cannot Extract File Size from the URL "
+    MAX_FILE_SIZE = "File size is greater than 2Gb. Which is the limit imposed by telegram!"
+    LONG_CUS_FILENAME = """Filename you provided is greater than 60 characters.
+Please provide a shorter name."""
+    UNSUPPORTED_FORMAT = "ERROR 🚫🚫 : File format {} Not supported!"
+    CHOOSE_CMD = "Subtitle file downloaded successfully!.\nChoose your desired mixing!\n[ /softremove , /softmux , /hardmux ]"
